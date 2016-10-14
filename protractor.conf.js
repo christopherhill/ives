a3f1f5ddf26d75b9166dfcd3741a03b1652dfc2e
Christopher Hill <accounts@christopherhill.com>
2016-10-14 14:20:50 -0700

    Add protractor (WIP).

---
+++ protractor.conf.js
@@ -0,0 +1,27 @@
+exports.config = {
+  baseUrl: 'http://localhost:8080/',
+  specs: [
+    './e2etests.ts'
+  ],
+  exclude: [],
+  framework: 'jasmine2',
+  allScriptsTimeout: 110000,
+  jasmineNodeOpts: {
+    showTiming: true,
+    showColors: true,
+    isVerbose: false,
+    includeStackTrace: false,
+    defaultTimeoutInterval: 400000
+  },
+  directConnect: true,
+  capabilities: {
+    'browserName': 'chrome',
+    'chromeOptions': {
+      'args': ['show-fps-counter=true']
+    }
+  },
+  onPrepare: function() {
+    browser.ignoreSynchronization = false;
+  },
+  useAllAngular2AppRoots: true
+};
